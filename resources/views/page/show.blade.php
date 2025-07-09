ce67af4733f1cf7d792e1fb9600cd9ede4cad103
Paul <fgocsgo@gmail.com>
2025-07-09 16:20:48 +0200

    style: base page view enhancements

--- resources/views/page/show.blade.php
+++ resources/views/page/show.blade.php
@@ -1,6 +1,8 @@
 <x-app-layout title="{{ $page->meta_title ?? $page->name }}">
-    <x-container>
-        <h1 class="mb-6">{{ $page->title_h1 ?? $page->name }}</h1>
-        {!! html_entity_decode($page->content) !!}
-    </x-container>
+    <div class="page py-6">
+        <x-container>
+            <h1 class="mb-6 text-2xl md:text-3xl font-semibold">{{ $page->title_h1 ?? $page->name }}</h1>
+            {!! html_entity_decode($page->content) !!}
+        </x-container>
+    </div>
 </x-app-layout>
