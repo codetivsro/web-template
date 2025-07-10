e0a5d1917428542c17ec8af73b8315f2dbc2dcb1
Paul <fgocsgo@gmail.com>
2025-07-10 14:09:00 +0200

    feat: add eng translation files

---
+++ lang/en/admin/resources.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'blocks' => [
+        'title' => [
+            'singular' => 'Block',
+            'plural' => 'Blocks',
+        ],
+    ],
+    'menus' => [
+        'title' => [
+            'singular' => 'Menu',
+            'plural' => 'Menu',
+        ],
+    ],
+    'pages' => [
+        'title' => [
+            'singular' => 'Page',
+            'plural' => 'Pages',
+        ],
+    ],
+];
